4c2bfb034a1551af6f3f6fa8256f344780fde63a
ha-hao <haotian3gy@gmail.com>
2017-04-22 19:24:23 -0400

    get num of cat

--- test.m
+++ test.m
@@ -1,1 +1,30 @@
-M = csvread('Strain.csv');
+s_original = readtable('Strain.csv');
+
+d = readtable('destinations.csv');
+
+s = s_original;
+s.site_name = categorical(s.site_name);
+s.posa_continent = categorical(s.posa_continent);
+s.user_location_country = categorical(s.user_location_country);
+s.user_location_region = categorical(s.user_location_region);
+s.user_location_city = categorical(s.user_location_city);
+s.is_mobile = categorical(s.is_mobile);
+s.is_package = categorical(s.is_package);
+s.channel = categorical(s.channel);
+%s.srch_destination_id = categorical(s.srch_destination_id);
+s.srch_destination_type_id = categorical(s.srch_destination_type_id);
+s.is_booking = categorical(s.is_booking);
+s.hotel_continent = categorical(s.hotel_continent);
+s.hotel_country = categorical(s.hotel_country);
+s.hotel_market = categorical(s.hotel_market);
+s.hotel_cluster = categorical(s.hotel_cluster);
+
+for i = 1:size(s,2)
+    if iscategorical(table2array(s(:,i)))
+        disp(s.Properties.VariableNames{i});
+        disp(size(categories(table2array(s(:,i))),1));
+    end
+end
+
+
+
