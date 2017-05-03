659afe0cae7bf6520f5fe828966a03c8372865f5
ha-hao <haotian3gy@gmail.com>
2017-05-03 18:19:25 -0400

    tree

--- dec_tree.m
+++ dec_tree.m
@@ -1,4 +1,4 @@
-function [mdl] = dec_tree(X, Y, rule_n)
+function [mdl] = dec_tree(X, Y, rule_n, pruning)
 
 % Select Splitting rules
 rules = {'gdi', 'twoing', 'deviance'};
@@ -7,10 +7,13 @@ selected_rule = rules{rule_n};
 % Model construction
 tree = fitctree(X,Y, 'SplitCriterion', selected_rule);
 
-% Optimize Pruning Level
-[~,~,~,bestlevel] = cvLoss(tree,...
+if pruning == 1
+    % Optimize Pruning Level
+    [~,~,~,bestlevel] = cvLoss(tree,...
     'SubTrees','All','TreeSize','min');
     
-% Pruning
-mdl = prune(tree,'Level',bestlevel);
+    % Pruning
+    mdl = prune(tree,'Level',bestlevel);
+end 
+
 end
